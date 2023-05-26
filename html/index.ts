1f346e2dacf25fa73bb811dc50cb9809af3e5a47
AaravSibbal <106462208+AaravSibbal@users.noreply.github.com>
2023-05-26 01:03:09 -0400

    First Commit
    This is really just the barebones right now took sometime to think of this one could be fun just want to make something different and learn more things like express, Typescript, promises and css in general might use bootstrap, but for now on this one I have made a clone of notepad not that it was a flex lol

---
+++ html/index.ts
@@ -0,0 +1,25 @@
+const cursor = document.getElementById("cursor")
+const fontSize = document.getElementById("font-size")
+
+function initailize(){
+    console.log("something")
+    populateFontSize()
+}
+function populateFontSize(){
+    //TODO: see if there is any default values you can set on this
+    let count = 2
+    if(fontSize !== null){
+        for(let i=0; i<6; i++){ 
+            if(i=0){
+                fontSize.innerHTML += `<option>1</option>`
+            }
+            fontSize.innerHTML += `<option>${count**i}</option>`
+        }
+    }
+    
+}
+
+window.onload = ()=>{
+    console.log("bom")
+    initailize()
+}
